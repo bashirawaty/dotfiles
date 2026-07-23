7de29879248d5cb4924415975fce569fc981d12b
bashirawaty <94090110+bashirawaty@users.noreply.github.com>
2026-07-24 00:08:46 +1000

    Configure Python host for macOS and Unix
    Set Python host program for macOS and Unix.

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -13,3 +13,11 @@ opt.signcolumn = "yes"
 opt.scrolloff = 8
 opt.sidescrolloff = 8
 opt.clipboard = "unnamedplus"
+
+-- macOS Python host (Homebrew)
+if vim.fn.has("mac") == 1 then
+  vim.g.python3_host_prog = "/opt/homebrew/bin/python3"
+elseif vim.fn.has("unix") == 1 then
+  vim.g.python3_host_prog = "/usr/bin/python3"
+end
+
