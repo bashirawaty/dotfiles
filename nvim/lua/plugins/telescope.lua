179a68872afff24a5328abe62aeccda405b70d46
bashirawaty <94090110+bashirawaty@users.noreply.github.com>
2026-07-24 00:29:34 +1000

    Configure Telescope with custom settings
    Updated telescope setup with custom defaults for vimgrep arguments and file ignore patterns.

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,35 @@
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+
   config = function()
     local telescope = require("telescope")
-    telescope.setup({})
+
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--hidden",
+          "--glob", "!**/.git/*",
+          "--glob", "!**/node_modules/*",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+        },
+
+        -- Cross‑platform path handling
+        file_ignore_patterns = {
+          "%.git/",
+          "node_modules/",
+        },
+      },
+    })
+
+    -- Keymaps
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
     vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>")
+    vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+    vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
   end,
 }
