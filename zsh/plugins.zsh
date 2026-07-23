b950974bb968b8c167406f75cf5f4f506ff01a75
bashirawaty <94090110+bashirawaty@users.noreply.github.com>
2026-07-24 00:42:48 +1000

    Add Zinit plugins for autosuggestions and syntax highlighting

---
+++ zsh/plugins.zsh
@@ -0,0 +1,8 @@
+### --- ZINIT PLUGINS ---
+zinit light zsh-users/zsh-autosuggestions
+zinit light zsh-users/zsh-syntax-highlighting
+zinit light zsh-users/zsh-completions
+
+### --- OPTIONAL ---
+ zinit light romkatv/powerlevel10k
+ [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
